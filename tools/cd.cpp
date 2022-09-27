edacbc720178afa6c2ef1a9b7f019c527f7d8c7f
Jan <41554065+NotKronos@users.noreply.github.com>
2022-09-27 18:56:02 +0200

    Create cd.cpp
    intital commit

---
+++ tools/cd.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <filesystem>
+
+int main(int argc, const char* argv[]) {
+    if(argc > 1) {
+        std::cerr << "[cd]: Too many arguments" << std::endl; 
+        return 1;
+    }
+
+    if (argc == 1) {
+        std::cerr << "[cd]: No arguments specified" << std::endl;
+        return 1;
+    }
+
+    std::string current_path = std::filesystem::current_path().string();
+    std::string arg(argv[1]);
+
+    if(arg.rfind("..", 0) == 0) {
+        std::filesystem::current_path(arg);
+    }
+
+    else {
+        std::filesystem::path path;
+        path = std::filesystem::current_path() / ("C:" + arg);
+
+        if(std::filesystem::is_directory(path)) 
+            std::filesystem::current_path(path);
+        else    
+            std::cerr << "[cd]: No such directory" << std::endl;
+    }
+    return 0;
+}
